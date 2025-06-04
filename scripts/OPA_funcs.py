Fix generation of samples. Apply normalization only before feeding to model (attack scripts)

scripts/OPA_funcs.py
@@ -3,7 +3,7 @@ from torch import nn
 import torch.nn.functional as F
 import numpy as np
 
-from utils.attack_aux_funcs import visualize_perturbations
+from utils.attack_aux_funcs import visualize_perturbations, normalize_cifar10
 
 
 class OnePixelAttack:
@@ -23,7 +23,8 @@ class OnePixelAttack:
         Parameters
         ----------
         model : Model used.
-        img   : Original image. Dimensions: [channels, height, width].
+        img   : Original image. Dimensions: [channels, height, width]. In our implementation, img is such that it has been
+        applied only ToTensor transform, i.e., it is not normalized. Thus, it is expected to be in the range [0, 1].
         label : Real label for the image.
         n     : Size of the population.
         target_label : Target label for the attack. If None, it is a non-targeted attack.
@@ -45,7 +46,7 @@ class OnePixelAttack:
         self.target_idx = self.target_label if self.target_label is not None else self.label
         self.fitness = self._evaluate_population()
         with torch.no_grad():
-            prob = self.model(img.unsqueeze(0)).squeeze()
+            prob = self.model(normalize_cifar10(self.img).unsqueeze(0).to(self.device)).squeeze()
         self.historical_fitness = [F.softmax(prob, dim=0)
             [self.target_idx].item()]
 
@@ -114,7 +115,7 @@ class OnePixelAttack:
         ]).to(self.device)
 
         with torch.no_grad():
-            logits = self.model(perturbed_imgs)
+            logits = self.model(normalize_cifar10(perturbed_imgs).to(self.device))
             probs = F.softmax(logits, dim=1)
             # return probs[:, self.label].detach().cpu().unsqueeze(1)
             return probs[:, self.target_idx].unsqueeze(1)
@@ -145,7 +146,7 @@ class OnePixelAttack:
 
         # Calculate the fitness of using this child
         with torch.no_grad():
-            logits = self.model(perturbed_imgs)
+            logits = self.model(normalize_cifar10(perturbed_imgs).to(self.device))
             probs = F.softmax(logits, dim=1)
             new_fitnesses = probs[:, self.target_idx].unsqueeze(1)
 
@@ -230,8 +231,8 @@ class OnePixelAttack:
                 # Trigger early stopping for targeted attacks. In order to avoid checking too often, we do so every 5 epochs
                 if self.target_label is not None and i % 5 == 0:
                     # Find current best perturbation
-                    min_fitness_idx = torch.argmin(self.fitness, dim=0)
-                    best_p = self.population[min_fitness_idx].squeeze()
+                    best_fitness_idx = torch.argmax(self.fitness, dim=0)
+                    best_p = self.population[best_fitness_idx].squeeze()
                     # Apply it temporarily to check for predictions
                     current_img = self._add_perturbation(
                         [best_p], self.perturbed_img
@@ -239,7 +240,8 @@ class OnePixelAttack:
 
                     with torch.no_grad():
                         # Obtain class logits for the perturbed image
-                        prob = self.model(current_img.unsqueeze(0)
+                        prob = self.model(
+                            normalize_cifar10(current_img).unsqueeze(0).to(self.device)
                         ).squeeze()
 
                         if not torch.isnan(prob).any():
@@ -262,8 +264,8 @@ class OnePixelAttack:
 
             # Obtain the pixel of the population that better perturbes the
             # image for j number of perturbations
-            min_fitness_idx = torch.argmin(self.fitness, dim=0)
-            best_p = self.population[min_fitness_idx].squeeze()
+            best_fitness_idx = torch.argmax(self.fitness, dim=0) if self.target_label is not None else torch.argmin(self.fitness, dim=0)
+            best_p = self.population[best_fitness_idx].squeeze()
             # Add the best perturbation to the list
             perturbations.append(best_p)
 

scripts/attack_main.py
@@ -10,8 +10,8 @@ import warnings
 
 # Auxiliary scripts
 from scripts.OPA_funcs import OnePixelAttack
-from utils.attack_aux_funcs import visualize_perturbations, CIFAR_LABELS
-from config import IMAGES, MODELS_DICT, PRETRAINED_MODELS
+from utils.attack_aux_funcs import visualize_perturbations, normalize_cifar10
+from config import IMAGES, MODELS_DICT, PRETRAINED_MODELS, TEST_SET, CIFAR_LABELS
 
 # IMAGES_PATH = './data/images'
 
@@ -108,7 +108,7 @@ def get_valid_images(dataset, model_name, device, num_images=10):
 
         img = img.to(device)
         with torch.no_grad():
-            pred = torch.argmax(model(img.unsqueeze(0)), dim=1).item()
+            pred = torch.argmax(model(normalize_cifar10(img).unsqueeze(0).to(device)), dim=1).item()
         if pred == label:  # Correct prediction
             valid_images.append((img, label, idx))
             used_indices.add((idx, label))
@@ -128,9 +128,11 @@ def save_perturbation(perturbations, model_name, idx, original_label, target_lab
     # Extract components. Assume d=1
     H = W = 32
     best_p = perturbations[0]
+    print(f"Best perturbation: {best_p}, type: {type(best_p)}")
     row, col = int(round(best_p[0].item() * H)), int(round(best_p[1].item() * W))
     rgb = best_p[2:].detach().cpu()
 
+    print(f"Saving perturbation at pixel ({row}, {col}) with RGB values {rgb}")
     payload = {
         'row': row,
         'col': col,
@@ -149,13 +151,6 @@ def main(model_name, n=400, epochs=100, num_images=10, device='cuda'):
     model.eval()  # Set model to evaluation mode
     print(f"Loaded model: {model_name} from {model_path}")
     print(f"Model architecture: {model}")
-
-    # Load CIFAR-10 dataset
-    test_transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
-    ])
-    test_dataset = CIFAR10(root='./data', train=False, download=False, transform=test_transform)
 
     # # Obtain a random image and its label
     # idx = int(torch.randint(0, len(test_dataset), (1,)).item())
@@ -200,7 +195,7 @@ def main(model_name, n=400, epochs=100, num_images=10, device='cuda'):
     #         print(f"Total time: {visualize_time - start:.2f} seconds")
 
     # Select new images for the attack
-    samples = get_valid_images(test_dataset, model_name, device, num_images=num_images)
+    samples = get_valid_images(TEST_SET, model_name, device, num_images=num_images)
     print(f"Selected {len(samples)} valid images for the attack.")
 
     for img, label, idx in samples:
@@ -209,7 +204,7 @@ def main(model_name, n=400, epochs=100, num_images=10, device='cuda'):
         # Save the original image
         save_original_image(img, label, model_name, idx)
 
-        original_pred = torch.argmax(model(img.unsqueeze(0).to(device)), dim=1).item()
+        # original_pred = torch.argmax(model(img.unsqueeze(0).to(device)), dim=1).item()
 
         for target_label in range(10):
             if target_label != label:
@@ -225,12 +220,12 @@ def main(model_name, n=400, epochs=100, num_images=10, device='cuda'):
 
                 # Verify attack success
                 with torch.no_grad():
-                    perturbed_pred = torch.argmax(model(perturbed_img.unsqueeze(0).to(device)), dim=1).item()
+                    perturbed_pred = torch.argmax(model(normalize_cifar10(perturbed_img).unsqueeze(0).to(device)), dim=1).item()
 
                 # verify_time = time.perf_counter()
                 # print(f"Verification time: {verify_time - attack_time:.2f} seconds")
 
-                print(f"Original prediction: {original_pred}, Perturbed prediction: {perturbed_pred}")
+                print(f"Original prediction: {label}, Perturbed prediction: {perturbed_pred}")
 
                 # Visualize perturbations
                 visualize_perturbations(
@@ -240,6 +235,8 @@ def main(model_name, n=400, epochs=100, num_images=10, device='cuda'):
                     target_label=target_label,
                     perturbed_label=perturbed_pred
                 )
+
+                print(f"Final perturbation: {perturbations[0]}") 
 
                 # Save perturbation
                 save_perturbation(perturbations, model_name, idx, label, target_label, perturbed_pred)

utils/attack_aux_funcs.py
@@ -42,9 +42,9 @@ def _add_information(
     with contextlib.redirect_stdout(output):
         print(f"True Label: {real_class}")
         with torch.no_grad():
-            y_pred = F.softmax(model(img.unsqueeze(0)).squeeze(), dim=0)
+            y_pred = F.softmax(model(normalize_cifar10(img.unsqueeze(0)).squeeze()), dim=0)
             y_pred_perturbed = F.softmax(
-                model(perturbed_img.unsqueeze(0)).squeeze(), dim=0
+                model(normalize_cifar10(perturbed_img.unsqueeze(0)).squeeze()), dim=0
             )
 
         original_prediction = CIFAR_LABELS[torch.argmax(y_pred)]

utils/load_model.py
@@ -35,6 +35,7 @@ def load_perturbation(file_path: str, img: torch.Tensor) -> torch.Tensor:
     Returns
         - perturbed_img (torch.Tensor): The original image with the perturbation applied.        
     """
+    print(f"Loading perturbation from {file_path}...")
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"File {file_path} does not exist.")
 
@@ -43,6 +44,7 @@ def load_perturbation(file_path: str, img: torch.Tensor) -> torch.Tensor:
     perturbation = torch.load(file_path, map_location='cpu')
     row = perturbation['row']
     col = perturbation['col']
+    print(f"Perturbing pixel at ({row}, {col}) with RGB values {perturbation['rgb']}")
     p_img[:, row, col] = perturbation['rgb']
     return p_img
 
@@ -59,7 +61,7 @@ def get_model(model_name, checkpoint_path, device: str, num_classes=10):
     except KeyError:
         raise ValueError(f"Model '{model_name}' is not recognized. Available models: {list(MODELS_DICT.keys())}")
 
-    checkpoint = torch.load(checkpoint_path, map_location=device)
+    checkpoint = torch.load(checkpoint_path, map_location=device, weights_only=True)
 
     try:
         model.load_state_dict(checkpoint['model_state'])
@@ -99,77 +101,77 @@ def get_accuracy(model):
 
 
 def check_adversarial_samples(model, model_name, label: str, device='cpu'):
-    model_images_directory = os.path.join(IMAGES, model_name, label)
-    if not os.path.exists(model_images_directory):
-        raise FileNotFoundError(f"Directory {model_images_directory} does not exist.")
+    model_directory = os.path.join(IMAGES, model_name, label)
+    if not os.path.exists(model_directory):
+        raise FileNotFoundError(f"Directory {model_directory} does not exist.")
 
-    # List the different images (idx) in the directory if starts with number, not a character
-    images = [f for f in os.listdir(model_images_directory) if f[0].isdigit() and f.endswith('.png')]
-    if not images:
-        raise FileNotFoundError(f"No images found in {model_images_directory}.")
-    images.sort()  # Sort to ensure consistent order
-    print(f"Found {len(images)} images in {model_images_directory}.")
-    print(f"Images: {images}")
+    # List the different .pt files (idx) in the directory, i.e., the perturbations
+    perturbation_files = [f for f in os.listdir(model_directory) if f.endswith('.pt')]
+    if not perturbation_files:
+        raise FileNotFoundError(f"No perturbation files found in {model_directory}.")
+    perturbation_files.sort()  # Sort to ensure consistent order
+    print(f"Found {len(perturbation_files)} images in {model_directory}.")
+    print(f"Images: {perturbation_files}")
 
     # Pass the original image ({idx}_original.png) through the model
-    for image_name in images:
-        idx = image_name.split('_')[0]
-        if 'original' in image_name:
+    for perturbation_file in perturbation_files:
+        idx = perturbation_file.split('_')[0]  # Extract the index from the filename
+        # if 'original' in image_name:
 
-            # Obtain the image in the test set
-            original_image = TEST_SET[int(idx)][0].unsqueeze(0).to(device)
-            original_label = TEST_SET[int(idx)][1]
-            print(f"Original image {idx} label: {original_label}")
+        #     # Obtain the image in the test set
+        #     original_image = TEST_SET[int(idx)][0].unsqueeze(0).to(device)
+        #     original_label = TEST_SET[int(idx)][1]
+        #     print(f"Original image {idx} label: {original_label}")
 
-            # Get the model prediction
-            model.eval()
-            with torch.no_grad():
-                output = model(original_image)
-                pred_label = output.argmax(dim=1).item()
-                print(f"Model prediction for original image {idx}: {pred_label}, original label: {original_label}")
+        #     # Get the model prediction
+        #     model.eval()
+        #     with torch.no_grad():
+        #         output = model(original_image)
+        #         pred_label = output.argmax(dim=1).item()
+        #         print(f"Model prediction for original image {idx}: {pred_label}, original label: {original_label}")
             
-            # Obtain the original image as saved in the directory
-            original_image_path = os.path.join(model_images_directory, image_name)
-            if not os.path.exists(original_image_path):
-                raise FileNotFoundError(f"Image {original_image_path} does not exist.")
-            original_image_saved = TEST_TRANSFORM(Image.open(original_image_path).convert('RGB')).unsqueeze(0).to(device)
-            original_img_unnorm = undo_normalization_cifar10(original_image_saved).squeeze(0)
-            with torch.no_grad():
-                output_saved = model(original_image_saved)
-                pred_label_saved = output_saved.argmax(dim=1).item()
-                print(f"Model prediction for saved original image {idx}: {pred_label_saved}, original label: {original_label}")
-        else:
-            values = image_name.split('_')
-            # Obtain the perturbed image ({idx}_perturbed.png) in the test set
-            perturbed_image = TEST_TRANSFORM(Image.open(os.path.join(model_images_directory, image_name)).convert('RGB')).unsqueeze(0).to(device)
-            perturbed_image_unnorm = undo_normalization_cifar10(perturbed_image).squeeze(0)
-            perturbed_label = int(values[4].split('.')[0])
-            print(f"Perturbed image {idx} label: {perturbed_label}")
-            # Get the model prediction
-            model.eval()
-            with torch.no_grad():
-                output = model(perturbed_image)
-                pred_label = output.argmax(dim=1).item()
-                print(f"Model prediction for perturbed image {idx}: {pred_label}, perturbed label: {perturbed_label}")
+        #     # Obtain the original image as saved in the directory
+        #     original_image_path = os.path.join(model_images_directory, image_name)
+        #     if not os.path.exists(original_image_path):
+        #         raise FileNotFoundError(f"Image {original_image_path} does not exist.")
+        #     original_image_saved = TEST_TRANSFORM(Image.open(original_image_path).convert('RGB')).unsqueeze(0).to(device)
+        #     original_img_unnorm = undo_normalization_cifar10(original_image_saved).squeeze(0)
+        #     with torch.no_grad():
+        #         output_saved = model(original_image_saved)
+        #         pred_label_saved = output_saved.argmax(dim=1).item()
+        #         print(f"Model prediction for saved original image {idx}: {pred_label_saved}, original label: {original_label}")
+        values = perturbation_file.split('_')
+        # Obtain the perturbed image ({idx}_perturbed.png) in the test set
+        # perturbed_image = TEST_TRANSFORM(Image.open(os.path.join(model_images_directory, image_name)).convert('RGB')).unsqueeze(0).to(device)
+        perturbed_image = load_perturbation(os.path.join(model_directory, perturbation_file), TEST_SET[int(idx)][0])
+        # perturbed_image_unnorm = undo_normalization_cifar10(perturbed_image).squeeze(0)
+        perturbed_label = int(values[4].split('.')[0])
+        print(f"Perturbed image {idx} label: {perturbed_label}")
+        # Get the model prediction
+        model.eval()
+        with torch.no_grad():
+            output = model(normalize_cifar10(perturbed_image).unsqueeze(0).to(device))
+            pred_label = output.argmax(1).item()
+            print(f"Model prediction for perturbed image {idx}: {pred_label}, perturbed label: {perturbed_label}")
 
-            # Calculate the difference between the original and perturbed images
-            pixel_diff = (original_img_unnorm - perturbed_image_unnorm).abs()
+        # # Calculate the difference between the original and perturbed images
+        # pixel_diff = (original_img_unnorm - perturbed_image_unnorm).abs()
 
-            # Masking
-            threshold = 1e-4
-            mask = (pixel_diff > threshold).any(dim=0)  # Any channel differs
-            num_changed_pixels = mask.sum().item()
-            print(f"Number of changed pixels in image {idx}: {num_changed_pixels}")
+        # # Masking
+        # threshold = 1e-4
+        # mask = (pixel_diff > threshold).any(dim=0)  # Any channel differs
+        # num_changed_pixels = mask.sum().item()
+        # print(f"Number of changed pixels in image {idx}: {num_changed_pixels}")
 
-            if num_changed_pixels > 1:
-                print(f"Label {perturbed_label} has more than one pixel changed")
-                changed = torch.nonzero(mask)
-                for x, y in changed:
-                    orig_pixel = original_img_unnorm[:, x, y]
-                    perturbed_pixel = perturbed_image_unnorm[:, x, y]
-                    print(f"Pixel changed at ({x.item()}, {y.item()}): "
-                          f"Original: {[round(v.item(), 3) for v in orig_pixel]}, "
-                          f"Perturbed: {[round(v.item(), 3) for v in perturbed_pixel]}")
+        # if num_changed_pixels > 1:
+        #     print(f"Label {perturbed_label} has more than one pixel changed")
+        #     changed = torch.nonzero(mask)
+        #     for x, y in changed:
+        #         orig_pixel = original_img_unnorm[:, x, y]
+        #         perturbed_pixel = perturbed_image_unnorm[:, x, y]
+        #         print(f"Pixel changed at ({x.item()}, {y.item()}): "
+        #                 f"Original: {[round(v.item(), 3) for v in orig_pixel]}, "
+        #                 f"Perturbed: {[round(v.item(), 3) for v in perturbed_pixel]}")
 
 
 if __name__ == '__main__':
@@ -180,7 +182,7 @@ if __name__ == '__main__':
     # get_accuracy(model)
 
     # Check adversarial samples
-    check_adversarial_samples(model, args.model, 'frog', 'cpu')
+    check_adversarial_samples(model, args.model, 'truck', 'cpu')
 
     # How to use this script: 
     # python -m utils.load_model --model conv_allconv --chckpt_path ./results/allconv.pth